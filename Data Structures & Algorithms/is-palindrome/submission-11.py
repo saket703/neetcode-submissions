eb5687c690d793ea786fbf39de9ba114fc3024b8
saket703 <70266393+saket703@users.noreply.github.com>
2026-09-19 22:36:53 +0530

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        LP=0
+        RP=len(s)-1
+        while LP<RP:
+            if s[LP].isalpha()==False and s[LP].isdigit()==False:
+                LP+=1
+            elif s[RP].isalpha()==False and s[RP].isdigit()==False:
+                RP-=1
+                
+            elif s[LP].lower()==s[RP].lower():
+                LP+=1
+                RP-=1
+                continue
+            else:
+                return False
+        
+        return True
