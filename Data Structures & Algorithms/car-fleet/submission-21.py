8d54b4e2c5b563cb9c0e47518b0216e81bcf8518
saket703 <70266393+saket703@users.noreply.github.com>
2026-09-23 19:52:16 +0530

    Add: car-fleet - submission-21

---
+++ Data Structures & Algorithms/car-fleet/submission-21.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pands={}
+        threshold=0
+        x=len(speed)
+        fleets=x
+        for i in range(0,len(position)):
+            pands[position[i]]=speed[i]
+        position.sort(reverse=True)
+        pands=dict(sorted(pands.items(),reverse=True))
+        speed=list(pands.values())
+        time=[(target-position[i])/speed[i] for i in range(0,x)]
+        for i in range(0,x-1):
+            if time[threshold]>=time[i+1]:
+                fleets-=1
+            else:
+                threshold=i+1
+        return fleets
